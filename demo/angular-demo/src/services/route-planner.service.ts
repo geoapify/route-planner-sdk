157b77e1e9eb72509497181b2b3eefef9c613109
Oleg Hovsepyan <hovsepyan021@gmail.com>
2025-04-27 19:50:17 +0400

    GD-80 (DRAFT): Move the Timeline generation code into route-planner-sdk

--- demo/angular-demo/src/services/route-planner.service.ts
+++ demo/angular-demo/src/services/route-planner.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import RoutePlanner, {Agent, Job} from '../../../..';
+import RoutePlanner, { Agent, Job, RoutePlannerInputData } from '../../../..';
 import TEST_API_KEY from "../../../../env-variables";
 // import RoutePlanner, { Agent, Job } from "@geoapify/route-planner-sdk";
 
@@ -16,6 +16,18 @@ export class RoutePlannerService {
           .setMode("drive")
           .addAgent(new Agent().setId("agent-1").setStartLocation(13.38, 52.52))
           .addJob(new Job().setId("job-1").setLocation(13.39, 52.51))
+          .plan();
+    } catch (error) {
+      console.error("API test failed:", error);
+      return "Error connecting to API";
+    }
+  }
+
+  async planRoute(rawData: RoutePlannerInputData) {
+    try {
+      const planner = new RoutePlanner({apiKey: this.API_KEY});
+      return await planner
+          .setRaw(rawData)
           .plan();
     } catch (error) {
       console.error("API test failed:", error);
