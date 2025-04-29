becc960447587e67b78edbb1f4b92361aa241415
Oleg Hovsepyan <hovsepyan021@gmail.com>
2025-04-30 00:24:47 +0400

    GD-80 (DRAFT): Move the Timeline generation code into route-planner-sdk
    *Changed RoutePlannerTimeline to use RoutePlannerResult (with getters)
    *Changed the logic to get everything via constructor
    *Changed the models used
    *Updated the demo

--- src/models/interfaces/timeline/solution-item.ts
+++
@@ -1,8 +0,0 @@
-export interface SolutionItem {
-  position: string;
-  actualWidth: string;
-  minWidth: string;
-  form: 'full' | 'standard' | 'minimal';
-  type: 'start' | 'storage' | 'job' | 'end';
-  description: string;
-}
