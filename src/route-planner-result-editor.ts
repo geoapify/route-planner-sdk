b5e9337044b4e60c2ad6a2b7ba6d7a4874511d41
Oleg Hovsepyan <hovsepyan021@gmail.com>
2025-05-13 00:50:47 +0400

    GD-85: Bugfix
    *Issue 2: Adding array validations

--- src/route-planner-result-editor.ts
+++ src/route-planner-result-editor.ts
@@ -19,6 +19,7 @@ export class RoutePlannerResultEditor {
      * @returns {boolean} - Returns true if the job was successfully assigned.
      */
     async assignJobs(agentId: string, jobIds: string[]): Promise<boolean> {
+        this.assertArray(jobIds, "jobIds");
         return new RouteResultJobEditor(this.result).assignJobs(agentId, jobIds);
     }
 
@@ -29,6 +30,7 @@ export class RoutePlannerResultEditor {
      * @returns {boolean} - Returns true if the shipment was successfully assigned.
      */
     async assignShipments(agentId: string, shipmentIds: string[]): Promise<boolean> {
+        this.assertArray(shipmentIds, "shipmentIds");
         return new RouteResultShipmentEditor(this.result).assignShipments(agentId, shipmentIds);
     }
 
@@ -38,6 +40,7 @@ export class RoutePlannerResultEditor {
      * @returns {boolean} - Returns true if the job was successfully removed.
      */
     async removeJobs(jobIds: string[]): Promise<boolean> {
+        this.assertArray(jobIds, "jobIds");
         return new RouteResultJobEditor(this.result).removeJobs(jobIds);
     }
 
@@ -47,6 +50,7 @@ export class RoutePlannerResultEditor {
      * @returns {boolean} - Returns true if the shipment was successfully removed.
      */
     async removeShipments(shipmentIds: string[]): Promise<boolean> {
+        this.assertArray(shipmentIds, "shipmentIds");
         return new RouteResultShipmentEditor(this.result).removeShipments(shipmentIds);
     }
 
@@ -57,6 +61,7 @@ export class RoutePlannerResultEditor {
      * @returns {boolean} - Returns true if jobs were successfully added.
      */
     addNewJobs(agentId: string, jobs: Job[]): Promise<boolean> {
+        this.assertArray(jobs, "jobs");
         return new RouteResultJobEditor(this.result).addNewJobs(agentId, jobs);
     }
 
@@ -67,6 +72,7 @@ export class RoutePlannerResultEditor {
      * @returns {boolean} - Returns true if shipments were successfully added.
      */
     addNewShipments(agentId: string, shipments: Shipment[]): Promise<boolean> {
+        this.assertArray(shipments, "shipments");
         return new RouteResultShipmentEditor(this.result).addNewShipments(agentId, shipments);
     }
 
@@ -76,5 +82,11 @@ export class RoutePlannerResultEditor {
      */
     getModifiedResult(): RoutePlannerResult {
         return this.result;
+    }
+
+    private assertArray(array: any[], name: string): void {
+        if (!Array.isArray(array)) {
+            throw new Error("Type error: " + name + " must be an array");
+        }
     }
 }
