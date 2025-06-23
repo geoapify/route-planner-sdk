36eb8eb60d87bc594a5531340ed77057cd22a5cd
Alfiya Tarasenko <alfiya.tarasenko@geoapify.com>
2025-06-23 14:06:39 +0300

    Add color to tree dots in timeline

--- src/route-planner-timeline.ts
+++ src/route-planner-timeline.ts
@@ -28,7 +28,7 @@ export class RoutePlannerTimeline {
             <div class="geoapify-rp-sdk-three-dot-menu" data-agent-index="${timeline.agentIndex}">
                 <button class="geoapify-rp-sdk-three-dot-button">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512">
-                        <path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/>
+                        <path fill="currentColor" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/>
                     </svg>
                 </button>
                 <ul class="geoapify-rp-sdk-menu-list">
